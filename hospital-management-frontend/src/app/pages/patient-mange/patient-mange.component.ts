ad621bb57aee5993668e649c643d7e880a11b8e1
thisaramax20 <thisarathejana20@gmail.com>
2024-11-10 12:00:00 +0530

    sweet alert integrated

--- hospital-management-frontend/src/app/pages/patient-mange/patient-mange.component.ts
+++ hospital-management-frontend/src/app/pages/patient-mange/patient-mange.component.ts
@@ -1,11 +1,12 @@
 import { NgFor } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-patient-mange',
   standalone: true,
-  imports: [NgFor,HttpClientModule],
+  imports: [NgFor],
   templateUrl: './patient-mange.component.html',
   styleUrl: './patient-mange.component.css'
 })
@@ -19,6 +20,31 @@ export class PatientMangeComponent {
     this.http.get('http://localhost:8080/patient/get-all').subscribe((data:any) => {
       this.patientList = data;
     });
+  }
+
+  deletePatient(id:any){
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http.delete(`http://localhost:8080/patient/delete-by-id/${id}`).subscribe(() => {
+          this.loadPatientList();
+        });
+        Swal.fire({
+          title: "Deleted!",
+          text: "Patient has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+    
+    
   }
 
 }
