2339c7cb84d594294ae446d63181fe25035fc0cb
thisaramax20 <thisarathejana20@gmail.com>
2024-11-20 11:22:50 +0530

    report create done

--- hospital-management-frontend/src/app/pages/reports/add-report/add-report.component.ts
+++ hospital-management-frontend/src/app/pages/reports/add-report/add-report.component.ts
@@ -1,12 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Report } from '../../../model/Report';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-report',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-report.component.html',
   styleUrl: './add-report.component.css'
 })
 export class AddReportComponent {
+  public report:Report = new Report('', '', '', 0, 0, 0);
+  constructor(private readonly http: HttpClient){
+    
+  }
+  addReport(report: Report): void {
+    // Add your code to send the report to the server
+    const response = this.http.post('http://localhost:8080/report/save', report).subscribe();
+  }
 
 }
